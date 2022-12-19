deddc6b1132989ebe9408620365adf29ab06cd39
ondrej <ondrej.habala@gmail.com>
2022-12-19 10:26:35 +0100

    dorobene metody put, list

--- src/main/java/sk/uisav/icontrol/WebDavClient.java
+++ src/main/java/sk/uisav/icontrol/WebDavClient.java
@@ -1,19 +1,75 @@
 package sk.uisav.icontrol;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.StandardCopyOption;
+import java.util.LinkedList;
+import java.util.List;
 
+import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
 
 public class WebDavClient 
 {
+    /**
+     * Represents a WebDAV resource (a file or a directory)
+     */
+    public static class WebDavResource
+    {
+        /**
+         * The full name, including the path relative to the WebDAV content root, of a WebDAV resource
+         * @return full name, including its path, of a WebDAV resource
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * The content type of the WebDAV resource
+         * @return the content type of the WebDAV resources
+         */
+        public String getContentType() {
+            return contentType;
+        }
+
+        /**
+         * The size of the WebDAV resource (-1 for a directory)
+         * @return The size of the WebDAV resource (-1 for a directory)
+         */
+        public long getSize() {
+            return size;
+        }
+
+        /**
+         * Determines whether a WebDAV resource is a directory or not
+         * @return true if the resource is a directory, false otherwise
+         */
+        public boolean isDirectory()
+        {
+            return this.getContentType() == "httpd/unix-directory";
+        }
+
+        private final String name;
+        private final String contentType;
+        private final long size;
+        public WebDavResource(String name, String contentType, long size)
+        {
+            this.name = name;
+            this.contentType = contentType;
+            this.size = size;
+        }
+
+    }
     private String username;
     private String password;
     private String wdroot;
     private Sardine sardine;
+    private String wdpath;
 
     /**
      * Creates instance of a WebDAV client
@@ -21,16 +77,20 @@ public class WebDavClient
      * @param password password
      * @param wdroot the root URL of the WebDAV file system, for example https://dav.example.com/webdav.php
      */
-    public WebDavClient(String username, String password, String wdroot)
-    {
+    public WebDavClient(String username, String password, String wdroot) throws MalformedURLException {
         this.username = username;
         this.password = password;
+
         this.wdroot = wdroot;
         //we strip trailing file separator(s) from the WEebDAV root URL
         while((this.wdroot.charAt(this.wdroot.length()-1) == '/'))
             this.wdroot = this.wdroot.substring(0, this.wdroot.length()-1);
 
-        sardine = SardineFactory.begin(username, password);
+        URL url = new URL(this.wdroot);
+        this.wdpath = url.getPath();
+
+        sardine = SardineFactory.begin(this.username, this.password);
+        sardine.enablePreemptiveAuthentication("https://mesos.ui.sav.sk:444");
     }
 
     /**
@@ -38,7 +98,7 @@ public class WebDavClient
      * @param path
      * @return
      */
-    private String getFullUrl(String path)
+    public String getFullUrl(String path)
     {
         if(path.length() < 1)
         return wdroot;
@@ -76,12 +136,70 @@ public class WebDavClient
 
     /**
      * Uploads a local file to WebDAV
-     * @param local
-     * @param remote
+     * @param local the name of the local file to upload
+     * @param remote the name of the remote file to upload
+     * @throws IOException
      */
-    public void put(String local, String remote)
+    public void put(String local, String remote) throws IOException
     {
+        String fullName = this.getFullUrl(remote);
+        sardine.put(fullName, new File(local), "text/plain");
+    }
+
+    /**
+     * Lists the content of a remote directory 
+     * @param remoteDir the remote directory to list
+     * @return a list of all the resources (files and directories) in the remote directory
+    */
+    public List<WebDavResource> listResources(String remoteDir) throws IOException
+    {
+        List<DavResource> reslist = sardine.list(this.getFullUrl(remoteDir));
+        List<WebDavResource> retv = new LinkedList<WebDavResource>();
+        for(DavResource res: reslist)
+        {
+            WebDavResource newRes = new WebDavResource(res.getPath().substring(wdpath.length()), res.getContentType(), res.getContentLength());
+            retv.add(newRes);
+        }
+        return retv;
+    }
+
+    /**
+     * Return the content of a remote directory as just names
+     * @param remoteDir the remote directory to list
+     * @return a list of names of resources in the remote directory
+     * @throws IOException
+     */
+    public List<String> list(String remoteDir) throws IOException
+    {
+        List<WebDavResource> resList = this.listResources(remoteDir);
+        List<String> retv = new LinkedList<String>();
+        for(WebDavResource res: resList)
+            retv.add(res.getName());
+        return retv;
+    }
+
+    /**
+     * Deletes a remote file
+     * @param remote the file to delete
+     * @return true if the file was successfully deleted, false otherwise
+     */
+    public void del(String remote) throws IOException {
+        sardine.delete(this.getFullUrl(remote));
+    }
+
+    /**
+     * Finds out whether a remote file exists
+     * @param remote the remote file name to check for
+     * @return true if file with the given name exists on the remote server, false otherwise
+     */
+    public boolean exists(String remote) throws IOException {
+        return sardine.exists(this.getFullUrl(remote));
+    }
+
+    public boolean isDirectory(String remote) throws IOException {
+        List<DavResource> resList = sardine.list(this.getFullUrl(remote));
+        return true;
 
     }
-    
+
 }
--- src/test/java/sk/uisav/icontrol/WebDavClientTest.java
+++ src/test/java/sk/uisav/icontrol/WebDavClientTest.java
@@ -1,18 +1,19 @@
 package sk.uisav.icontrol;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
+import com.github.sardine.DavResource;
 import org.junit.Test;
-import sk.uisav.icontrol.Settings;
+import sk.uisav.icontrol.Settings;;import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class WebDavClientTest {
     @Test
     public void testGet() 
     {
-        WebDavClient wdc = new WebDavClient(Settings.MesosWDusername, Settings.MesosWDpassword, Settings.MesosWDroot);
         long ret = -1;
         try{
+            WebDavClient wdc = new WebDavClient(Settings.MesosWDusername, Settings.MesosWDpassword, Settings.MesosWDroot);
             ret = wdc.get("/Microstep/2020/02/07/2320/90_FULLHD/panasonic_fullhd_01-090-180-202002072320.jpg", "/home/ondrej/Desktop/x.jpg");
         }
         catch(Exception e)
@@ -24,6 +25,30 @@ public class WebDavClientTest {
 
     @Test
     public void testPut() {
+        try{
+            WebDavClient wdc = new WebDavClient(Settings.MesosWDusername, Settings.MesosWDpassword, Settings.MesosWDroot);
+            wdc.put("/etc/issue", "/Microstep/test/testfile.txt");
+        }
+        catch(Exception e)
+        {
+            fail("Exception thrown in put()");
+        }
+    }
 
+    @Test
+    public void testListResources() {
+        try {
+            WebDavClient wdc = new WebDavClient(Settings.MesosWDusername, Settings.MesosWDpassword, Settings.MesosWDroot);
+            List<WebDavClient.WebDavResource> resList = wdc.listResources("/");
+            assertNotNull(resList);
+            for(WebDavClient.WebDavResource res: resList)
+                System.out.printf("Name: %s Content-type: %s size: %d is-dir: %b\n", res.getName(), res.getContentType(), res.getSize(), res.isDirectory());
+        } catch (IOException e) {
+            fail("IOException: " + e.toString());
+        }
+    }
+
+    @Test
+    public void testIsDirectory() {
     }
 }
